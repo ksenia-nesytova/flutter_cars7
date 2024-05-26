f22a0f7e4907c2e112bcfdaa6cc06ee1963dc79e
Ksenia Nesytova <ksenia.nesytova@office.e2e4gu.ru>
2024-05-26 17:21:56 +0700

    add: first screen basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+            child: Column(
+          children: [
+            Title(color: Colors.green, child: Text('Творчество')),
+            Container(
+              height: 335,
+              width: 335,
+              decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(24.36)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Change pic');
+              },
+              child: Text('Поменять картинку'),
+            ),
+            Container(
+              height: 88,
+              width: 335,
+              color: Colors.black38,
+              child: Text("Дополнительная информация"),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print("Поделиться");
+                },
+                child: Text('Поделиться творением'))
+          ],
+        )
+            // child: Text('Hello World!'),
         ),
       ),
     );
